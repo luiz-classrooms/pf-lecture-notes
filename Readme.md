e2d24b99ab8675109577d520a1b528b25d57e887
David Sena <sena.ufc@gmail.com>
2021-04-15 22:36:03 -0300

    up

---
+++ Readme.md
@@ -0,0 +1,1 @@
+[VEJA ONLINE](https://rawcdn.githack.com/senapk/pf-lecture-notes/ee5ea16c7b3cc0fcdfe52e29bc2ae6e08d036189/notas.html)
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #pdflatex --shell-escape -synctex=1 -interaction=nonstopmode notas.tex
 #rm -r *.aux *.log *.out *.toc *.synctex.gz _minted-notas
-pandoc notas.tex --standalone --mathjax -o notas.html
+pandoc notas.tex --standalone --toc --mathjax -o notas.html
 #pandoc notas.tex -o Readme.md
 #sed -i 's/``` {.bash bgcolor="bg"}/```bash/g' Readme.md
 #sed -i 's/``` {.haskell bgcolor="bg"}/```haskell/g' Readme.md
