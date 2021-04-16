ee5ea16c7b3cc0fcdfe52e29bc2ae6e08d036189
David Sena <sena.ufc@gmail.com>
2021-04-15 22:32:06 -0300

    up

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #pdflatex --shell-escape -synctex=1 -interaction=nonstopmode notas.tex
 #rm -r *.aux *.log *.out *.toc *.synctex.gz _minted-notas
-#pandoc notes.tex -o Readme.md
-sed -i 's/``` {.bash bgcolor="bg"}/```bash/g' Readme.md
-sed -i 's/``` {.haskell bgcolor="bg"}/```haskell/g' Readme.md
+pandoc notas.tex --standalone --mathjax -o notas.html
+#pandoc notas.tex -o Readme.md
+#sed -i 's/``` {.bash bgcolor="bg"}/```bash/g' Readme.md
+#sed -i 's/``` {.haskell bgcolor="bg"}/```haskell/g' Readme.md
