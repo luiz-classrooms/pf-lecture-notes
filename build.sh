24843ca93cf4adb54dafa971c5ec4d1feb48ee06
David Sena <sena.ufc@gmail.com>
2021-04-18 13:35:34 -0300

    update para minted

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#pdflatex --shell-escape -synctex=1 -interaction=nonstopmode notas.tex
+pdflatex --shell-escape -synctex=1 -interaction=nonstopmode notas.tex
 #rm -r *.aux *.log *.out *.toc *.synctex.gz _minted-notas
-pandoc notas.tex --standalone --toc --mathjax -o notas.html
-#pandoc notas.tex -o Readme.md
-#sed -i 's/``` {.bash bgcolor="bg"}/```bash/g' Readme.md
-#sed -i 's/``` {.haskell bgcolor="bg"}/```haskell/g' Readme.md
